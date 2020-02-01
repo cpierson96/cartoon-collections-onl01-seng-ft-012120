d6150a52e3e48a1b386e50b6467ed918335e0d63
Claudia Pierson <c172@umbc.edu>
2020-02-01 19:01:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(name)
 end
 
 def summon_captain_planet(array)
-  array.collect{|word.
+  array.collect{|word.capitalize!"
 end
 
 def long_planeteer_calls# code an argument here
